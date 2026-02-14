9bec2211ab3c424ab346e674d3c97f778a51acf0
muskan <muskanmittal1524@gmail.com>
2026-02-14 18:49:57 +0530

    uplash image domin added in ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,8 +4,12 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com',
+        hostname: 'source.unsplash.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      }
     ],
   },
 };
